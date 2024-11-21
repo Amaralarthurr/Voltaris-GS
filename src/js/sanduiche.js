833aaf5107eeb82a71a46a0c003688eafa8cc748
Arthur <arthuramaral697@gmail.com>
2024-11-21 14:16:57 -0300

    Header

--- src/js/sanduiche.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById("menuButton");
-const mobileMenu = document.getElementById("mobileMenu");
-
-menuButton.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-});
